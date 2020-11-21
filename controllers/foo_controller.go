40e4d0d405748452542c252ddea36843285d17f6
Naka Masato <nakamasato1989@gmail.com>
2020-11-21 14:47:10 +0900

    [4. Implement reconcile in controller.go]

--- controllers/foo_controller.go
+++ controllers/foo_controller.go
@@ -19,7 +19,11 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -29,24 +33,158 @@ import (
 // FooReconciler reconciles a Foo object
 type FooReconciler struct {
 	client.Client
-	Log    logr.Logger
-	Scheme *runtime.Scheme
+	Log      logr.Logger
+	Scheme   *runtime.Scheme
+	Recorder record.EventRecorder
 }
 
 // +kubebuilder:rbac:groups=samplecontroller.k8s.io,resources=foos,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=samplecontroller.k8s.io,resources=foos/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;delete
+// +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
 func (r *FooReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("foo", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("foo", req.NamespacedName)
 
-	// your logic here
+	// 1. Get Foo object
+	var foo samplecontrollerv1alpha1.Foo
+	log.Info("fetching Foo Resource")
+	if err := r.Get(ctx, req.NamespacedName, &foo); err != nil {
+		log.Error(err, "unable to fetch Foo")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// 2. Delete old Deployment if exists
+	if err := r.cleanupOwnedResources(ctx, log, &foo); err != nil {
+		log.Error(err, "failed to clean up old Deployment resources for this Foo")
+		return ctrl.Result{}, err
+	}
+
+	// 3. Create or update Deployment that Foo manages
+	deploymentName := foo.Spec.DeploymentName
+	deploy := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deploymentName,
+			Namespace: req.Namespace,
+		},
+	}
+
+	if _, err := ctrl.CreateOrUpdate(ctx, r.Client, deploy, func() error {
+		replicas := int32(1)
+		if foo.Spec.Replicas != nil {
+			replicas = *foo.Spec.Replicas
+		}
+		deploy.Spec.Replicas = &replicas
+
+		labels := map[string]string{
+			"app":        "nginx",
+			"controller": req.Name,
+		}
+
+		if deploy.Spec.Selector == nil {
+			deploy.Spec.Selector = &metav1.LabelSelector{MatchLabels: labels}
+		}
+
+		if deploy.Spec.Template.ObjectMeta.Labels == nil {
+			deploy.Spec.Template.ObjectMeta.Labels = labels
+		}
+
+		containers := []corev1.Container{
+			{
+				Name:  "nginx",
+				Image: "nginx:latest",
+			},
+		}
+
+		if deploy.Spec.Template.Spec.Containers == nil {
+			deploy.Spec.Template.Spec.Containers = containers
+		}
+
+		if err := ctrl.SetControllerReference(&foo, deploy, r.Scheme); err != nil {
+			log.Error(err, "unable to set ownerReference from Foo to Deployment")
+			return err
+		}
+
+		return nil
+	}); err != nil {
+		log.Error(err, "unable to ensure deployment is correct")
+		return ctrl.Result{}, err
+	}
+
+	// 4. Update Foo's status
+	var deployment appsv1.Deployment
+	var deploymentNamespacedName = client.ObjectKey{Namespace: req.Namespace, Name: foo.Spec.DeploymentName}
+	if err := r.Get(ctx, deploymentNamespacedName, &deployment); err != nil {
+		log.Error(err, "unable to fetch Deployment")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+	availableReplicas := deployment.Status.AvailableReplicas
+	if availableReplicas == foo.Status.AvailableReplicas {
+		return ctrl.Result{}, nil
+	}
+	foo.Status.AvailableReplicas = availableReplicas
+
+	if err := r.Status().Update(ctx, &foo); err != nil {
+		log.Error(err, "unable to update Foo status")
+		return ctrl.Result{}, err
+	}
+
+	r.Recorder.Eventf(&foo, corev1.EventTypeNormal, "Updated", "Update foo.status.AvailableReplicas: %d", foo.Status.AvailableReplicas)
 
 	return ctrl.Result{}, nil
 }
 
+var (
+	deploymentOwnerKey = ".metadata.controller"
+	apiGVStr           = samplecontrollerv1alpha1.GroupVersion.String()
+)
+
 func (r *FooReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	if err := mgr.GetFieldIndexer().IndexField(&appsv1.Deployment{}, deploymentOwnerKey, func(rawObj runtime.Object) []string {
+		deployment := rawObj.(*appsv1.Deployment)
+		owner := metav1.GetControllerOf(deployment)
+		if owner == nil {
+			return nil
+		}
+		if owner.APIVersion != apiGVStr || owner.Kind != "Foo" {
+			return nil
+		}
+
+		return []string{owner.Name}
+	}); err != nil {
+		return err
+	}
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&samplecontrollerv1alpha1.Foo{}).
+		Owns(&appsv1.Deployment{}).
 		Complete(r)
+}
+
+func (r *FooReconciler) cleanupOwnedResources(ctx context.Context, log logr.Logger, foo *samplecontrollerv1alpha1.Foo) error {
+	log.Info("finding existing Deployments for Foo resource")
+
+	var deployments appsv1.DeploymentList
+	if err := r.List(ctx, &deployments, client.InNamespace(foo.Namespace), client.MatchingFields(
+		map[string]string{
+			deploymentOwnerKey: foo.Name,
+		},
+	)); err != nil {
+		return err
+	}
+
+	for _, deployment := range deployments.Items {
+		if deployment.Name == foo.Spec.DeploymentName {
+			continue
+		}
+
+		if err := r.Delete(ctx, &deployment); err != nil {
+			log.Error(err, "failed to delete Deployment resource")
+			return err
+		}
+
+		log.Info("delete deployment resource: " + deployment.Name)
+		r.Recorder.Eventf(foo, corev1.EventTypeNormal, "Deleted", "Deleted deployment %q", deployment.Name)
+	}
+	return nil
 }
