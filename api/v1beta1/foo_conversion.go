71a90e120470f5078fc5895d36a606ffac883293
Masato Naka <masatonaka1989@gmail.com>
2021-12-13 10:33:31 +0900

    8. Update v1beta1 api and add foo_conversion.go

---
+++ api/v1beta1/foo_conversion.go
@@ -0,0 +1,44 @@
+package v1beta1
+
+import (
+	"sigs.k8s.io/controller-runtime/pkg/conversion"
+
+	samplecontrollerv1alpha1 "github.com/nakamasato/foo-controller-kubebuilder/api/v1alpha1"
+)
+
+// ConvertTo converts thisFoo to the Hubversion (v1alpha1)
+func (src *Foo) ConvertTo(dstRaw conversion.Hub) error {
+	dst := dstRaw.(*samplecontrollerv1alpha1.Foo)
+
+	// ObjectMeta
+	dst.ObjectMeta = src.ObjectMeta
+
+	// Spec
+	dst.Spec.DeploymentName = src.Spec.DeploymentName
+	dst.Spec.Replicas = src.Spec.Replicas
+
+	// Status
+	dst.Status.AvailableReplicas = src.Status.AvailableReplicas
+
+	return nil
+}
+
+// ConvertFrom converts from the Hub version (v1alpha1) to this version.
+func (dst *Foo) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*samplecontrollerv1alpha1.Foo)
+
+	// just copy DeploymentName value to Foo
+	dst.Spec.Foo = src.Spec.DeploymentName
+
+	// ObjectMeta
+	// ObjectMeta
+	dst.ObjectMeta = src.ObjectMeta
+
+	// Spec
+	dst.Spec.DeploymentName = src.Spec.DeploymentName
+	dst.Spec.Replicas = src.Spec.Replicas
+
+	// Status
+	dst.Status.AvailableReplicas = src.Status.AvailableReplicas
+	return nil
+}
