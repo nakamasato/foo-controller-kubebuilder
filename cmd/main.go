ba2adb456f8fb8b78e326783ab4349bb076250de
Masato Naka <masatonaka1989@gmail.com>
2023-09-27 08:31:20 +0900

    5. Create webhook

--- cmd/main.go
+++ cmd/main.go
@@ -97,6 +97,12 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Foo")
 		os.Exit(1)
 	}
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&samplecontrollerv1alpha1.Foo{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Foo")
+			os.Exit(1)
+		}
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
